52137fe4b4631179659bd7f23432572ff9a8e68c
Sarah Vessels <cheshire137@gmail.com>
2017-05-13 15:12:55 -0500

    Render Play component on app load

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Play from './components/play'
+
+window.onload = function() {
+  const rootEl = document.getElementById('root')
+  const playTemplate = document.getElementById('game-board-template')
+  const play = new Play(rootEl, playTemplate)
+  play.render()
+}
