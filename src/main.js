1883fb6b8f4da32b4a8e4d34bfd1a2a37908ebc9
Sarah Vessels <cheshire137@gmail.com>
2017-05-13 13:04:22 -0500

    Set up Babel

---
+++ src/main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+
+let mainWindow
+
+function onTitleChange(event, prefix) {
+  let title = ''
+  if (typeof prefix === 'string' && prefix.length > 0) {
+    title += `${prefix} - `
+  }
+  title += app.getName()
+  mainWindow.setTitle(title)
+}
+
+function createWindow() {
+  const title = app.getName()
+  mainWindow = new BrowserWindow({ width: 800, height: 600, title })
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+}
+
+app.on('ready', () => {
+  app.setAppUserModelId('com.blicblocktron.app')
+
+  createWindow()
+
+  mainWindow.webContents.on('did-finish-load', () => {
+    ipcMain.on('title', onTitleChange)
+  })
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
